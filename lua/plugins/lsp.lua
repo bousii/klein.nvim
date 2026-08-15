2f84fb6dd7465572677980849508a1f765df8e2d
Owen Giles <owen.a.giles@gmail.com>
2026-08-15 15:26:53 -0500

    add proper lsp keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,14 +19,37 @@ vim.lsp.config["rust_analyzer"] = {
 	filetypes = { "rust" },
 	root_markers = { "Cargo.toml" },
 }
+
 vim.api.nvim_create_autocmd('LspAttach', {
 	group = vim.api.nvim_create_augroup('my.lsp', {}),
 	callback = function(args)
 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		local buf = args.buf
+
+		if client:supports_method('textDocument/definition') then
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = buf, desc = 'Go to definition' })
+		end
+
+		if client:supports_method('textDocument/declaration') then
+			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = buf, desc = 'Go to declaration' })
+		end
+
+		if client:supports_method('textDocument/references') then
+			vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = buf, desc = 'Go to references' })
+		end
+
+		if client:supports_method('textDocument/implementation') then
+			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = buf, desc = 'Go to implementation' })
+		end
+
+		if client:supports_method('textDocument/hover') then
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = buf, desc = 'Hover' })
+		end
+
 		if client:supports_method('textDocument/completion') then
 			local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
 			client.server_capabilities.completionProvider.triggerCharacters = chars
-			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+			vim.lsp.completion.enable(true, client.id, buf, { autotrigger = true })
 		end
 	end,
 })
